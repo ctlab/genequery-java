50a1155c0381ec12f09163ab786173c9437c1840
smolcoder <smolcoder@gmail.com>
2015-11-03 19:24:50 +0300

    Intersection test

---
+++ commons/src/test/java/com/genequery/commons/models/ModuleTest.java
@@ -0,0 +1,13 @@
+package com.genequery.commons.models;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Arbuzov Ivan.
+ */
+public class ModuleTest extends TestCase {
+
+    public void testIntersectionSize() throws Exception {
+
+    }
+}
